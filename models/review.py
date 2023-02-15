c47cbbb883b813fbba3e8444ef4019f601aaa079
webstarly <edidionge.ukem@gmail.com>
2023-02-15 00:57:20 +0000

    task

--- models/review.py
+++ models/review.py
@@ -6,13 +6,13 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-     """
+    """
      A subclass of BaseModel class
      Public class attributes:
       place_id:            (str) will be Place.id
       user_id:             (str) will be User.id
       text:                (str)
-      """
-      place_id = ""
-      user_id = ""
-      text = ""
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
